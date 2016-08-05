a81c7679e803fc8dd81b10b254112295a05725d8
Josh Perry <joshperry@outlook.com>
2016-08-05 20:04:38 +0100

    Added a few more unit tests

--- NUnitTests/NUnitTest1.cs
+++ NUnitTests/NUnitTest1.cs
@@ -15,7 +15,7 @@ namespace NUnitTests
         }
 
         [Test]
-        public void Opcode3XNN_ValidInput_CorrectOutput()
+        public void Opcode6XNN_ValidInput_CorrectOutput()
         {
             // Arrange
             var rom = new byte[]
@@ -36,6 +36,113 @@ namespace NUnitTests
             Assert.True(_emulator.VRegisters[0] == 0xFF);
             Assert.True(_emulator.VRegisters[1] == 0x01);
             Assert.True(_emulator.VRegisters[2] == 0x02);
+        }
+
+        [Test]
+        public void Opcode7XNN_ValidInput_CorrectOutput()
+        {
+            // Arrange
+            var rom = new byte[]
+            {
+                // Add 1 to VRegister 0 
+                0x70, 0x01,
+
+                // Add 2 to VRegister 1 
+                0x71, 0x02,
+
+                // Add 3 to VRegister 2
+                0x72, 0x03
+            };
+
+            _emulator.LoadGame(rom);
+
+            // Set initial register values
+            _emulator.VRegisters[0] = 0x00;
+            _emulator.VRegisters[1] = 0x01;
+            _emulator.VRegisters[2] = 0x02;
+
+            // Act
+            _emulator.EmulateCycle();
+            _emulator.EmulateCycle();
+            _emulator.EmulateCycle();
+
+            // Assert
+            Assert.True(_emulator.VRegisters[0] == 0x01);
+            Assert.True(_emulator.VRegisters[1] == 0x03);
+            Assert.True(_emulator.VRegisters[2] == 0x05);
+        }
+
+        [Test]
+        public void Opcode8XY0_ValidInput_CorrectOutput()
+        {
+            // Arrange
+            var rom = new byte[]
+            {
+                // Sets vregister 0 to vregister 1
+                0x80, 0x10
+            };
+
+            _emulator.LoadGame(rom);
+
+            // Set initial register values
+            _emulator.VRegisters[0] = 0x00;
+            _emulator.VRegisters[1] = 0x01;
+
+            // Act
+            _emulator.EmulateCycle();
+
+            // Assert
+            Assert.True(_emulator.VRegisters[0] == _emulator.VRegisters[1]);
+            Assert.True(_emulator.VRegisters[0] == 0x01);
+            Assert.True(_emulator.VRegisters[1] == 0x01);
+        }
+
+        [Test]
+        public void Opcode8XY1_ValidInput_CorrectOutput()
+        {
+            // Arrange
+            var rom = new byte[]
+            {
+                // Sets vregister 0 to vregister 0 | vregister 1
+                0x80, 0x11
+            };
+
+            _emulator.LoadGame(rom);
+
+            // Set initial register values
+            _emulator.VRegisters[0] = 0x02;
+            _emulator.VRegisters[1] = 0x03;
+
+            // Act
+            _emulator.EmulateCycle();
+
+            // Assert
+            Assert.True(_emulator.VRegisters[0] == (2 | 3));
+            Assert.True(_emulator.VRegisters[0] == 3);
+        }
+
+        [Test]
+        public void Opcode8XY2_ValidInput_CorrectOutput()
+        {
+            // Arrange
+            var rom = new byte[]
+            {
+                // Sets vregister 0 to vregister 0 & vregister 1
+                0x80, 0x12
+            };
+
+            _emulator.LoadGame(rom);
+
+            // Set initial register values
+            _emulator.VRegisters[0] = 0x02;
+            _emulator.VRegisters[1] = 0x03;
+
+            // Act
+            _emulator.EmulateCycle();
+
+            // Assert
+            Assert.True(_emulator.VRegisters[0] == (2 & 3));
+            Assert.True(_emulator.VRegisters[0] == 2);
         }
     }
 }
